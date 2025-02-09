87a60a56b45d89e6e3b640242238b83420a1529d
obogrelova <on.obogrelova@gmail.com>
2025-02-09 14:40:16 +0500

    Update file

---
+++ main.py
@@ -0,0 +1,71 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, FSInputFile
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+
+from config import TOKEN
+import sqlite3
+import logging
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+class Form(StatesGroup):
+    name = State()
+    age = State()
+    grade = State()
+
+def init_db():
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    grade TEXT NOT NULL)
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer('Привет! Как тебя зовут?')
+    await state.set_state(Form.name)
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer('Сколько тебе лет?')
+    await state.set_state(Form.age)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer('В каком классе учишься?')
+    await state.set_state(Form.grade)
+
+@dp.message(Form.grade)
+async def grade(message: Message, state: FSMContext):
+    await state.update_data(grade=message.text)
+    school_data = await state.get_data()
+
+    conn = sqlite3.connect('school_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+    INSERT INTO students (name, age, grade) VALUES (?, ?, ?)''', (school_data['name'], school_data['age'], school_data['grade']))
+    conn.commit()
+    conn.close()
+
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
